9cfbb02ea5187b3be4218f425d87ddc088a501e3
jeizquierdo <jander.ensenat@gmail.com>
2026-05-24 14:19:43 -0400

    update researcher agent to include detected_tasks in prompt and improve error messaging

--- agents/researcher.py
+++ agents/researcher.py
@@ -5,9 +5,9 @@ from langchain_core.output_parsers import StrOutputParser
 
 def create_researcher_agent(llm):
     
-    researcher_prompt = load_prompt("prompts/researcher_prompt.txt",["messages"], config.file_encoding)
+    researcher_prompt = load_prompt("prompts/researcher_prompt.txt",["messages", "detected_tasks"], config.file_encoding)
     
-    researcher_chain =researcher_prompt | llm | StrOutputParser()
+    researcher_chain = researcher_prompt | llm | StrOutputParser()
     
     return researcher_chain
 
@@ -17,9 +17,12 @@ def researcher_node_function(state: dict, llm) -> dict:
     researcher_chain = create_researcher_agent(llm)
      
     try:
-        researcher_result = researcher_chain.invoke({"messages": state.get("messages", [])})
+        researcher_result = researcher_chain.invoke({
+            "messages": state.get("messages", []),
+            "detected_tasks": state.get("detected_tasks", [])
+            })
     except Exception as e:
-        researcher_result = f"Classification failed: {str(e)}"
+        researcher_result = f"Research failed: {str(e)}"
     
     return {
             "search_method": researcher_result["search_method"],
@@ -28,6 +31,13 @@ def researcher_node_function(state: dict, llm) -> dict:
     } 
     
     
-def route_tasks(state):
-    return [Send(f"{t}_agent", {"task": t, "context": state["context"]}) 
+def route_tasks_researcher(state):
+    return [Send(f"{t}_agent", 
+                 {
+                    "message": state["message"],
+                    "context": state["context"],
+                    "sources": state["sources"],
+                    "validation_notes": state.get("validation_notes", {}).get(t, ""),
+                    "detected_tasks": state["detected_tasks"],
+                     }) 
             for t in state["detected_tasks"]]
